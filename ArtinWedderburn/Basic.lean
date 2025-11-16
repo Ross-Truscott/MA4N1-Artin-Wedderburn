21243194bb4e3662e3a4eb430488c69541652936
Ross-Truscott <rosstruscott03@gmail.com>
2025-11-16 18:22:30 +0000

    Renamed project file, added imports

--- ArtinWedderburn/Basic.lean
+++
@@ -1,3 +0,0 @@
-def hello := "world"
-
-def test := "test"
---
+++ ArtinWedderburn/Project.lean
@@ -0,0 +1,6 @@
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Algebra.Module.Basic
+import Mathlib.Algebra.Ring.Basic
+import Mathlib.Data.Matrix.Basic
+import Mathlib.Algebra.Ring.Equiv
+import Mathlib.Algebra.DirectSum.Module
