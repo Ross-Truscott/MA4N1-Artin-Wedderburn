975c863caadec868016a4888ba9c40589444fec1
Ross-Truscott <rosstruscott03@gmail.com>
2025-11-16 20:12:18 +0000

    Added imports and basic defs

--- ArtinWedderburn/Project.lean
+++ ArtinWedderburn/Project.lean
@@ -4,12 +4,25 @@ import Mathlib.Algebra.Ring.Basic
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Algebra.Ring.Equiv
 import Mathlib.Algebra.DirectSum.Module
-import Mathlib.RingTheory.SimpleModule.Basic (IsSimpleModule)
+import Mathlib.RingTheory.SimpleModule.Basic
+-- Remove SimpleModule.Basic later when I work out how to bring just the defintions
 
---Define a ring R and a simple R-module S.
+/-
+This section is on the Proof of Lemma 2 from the outline, which states
 
+Let S be a simple R-module and D = End_R(S). Define M = S^n. Then End_R(M) = M_n(D)
+-/
+
+namespace Lemma2
+
+--Define a ring R, a simple R-module S .
 variable (R : Type u_2) [Ring R] (S : Type u_4) [AddCommGroup S] [Module R S] [IsSimpleModule R S]
 
+-- Define the Endomorphism ring of S and M = S^n
+def D := Module.End R S
+def M := sorry -- How do I do this?
 
-#check R
-#check S
+
+
+
+end Lemma2
