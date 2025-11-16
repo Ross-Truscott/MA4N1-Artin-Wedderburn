b61c01abfdce264f60ea3de0729e37b1b084dff6
Ross-Truscott <rosstruscott03@gmail.com>
2025-11-16 20:09:19 +0000

    Added imports and basic defs

--- ArtinWedderburn/Project.lean
+++ ArtinWedderburn/Project.lean
@@ -4,3 +4,12 @@ import Mathlib.Algebra.Ring.Basic
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Algebra.Ring.Equiv
 import Mathlib.Algebra.DirectSum.Module
+import Mathlib.RingTheory.SimpleModule.Basic (IsSimpleModule)
+
+--Define a ring R and a simple R-module S.
+
+variable (R : Type u_2) [Ring R] (S : Type u_4) [AddCommGroup S] [Module R S] [IsSimpleModule R S]
+
+
+#check R
+#check S
