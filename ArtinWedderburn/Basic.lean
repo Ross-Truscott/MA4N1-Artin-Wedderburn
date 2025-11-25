b834acfb6f421448bde2ad1ff6263962fa0d86ce
Ross-Truscott <rosstruscott03@gmail.com>
2025-11-25 17:00:22 +0000

    More tidying

--- ArtinWedderburn/Basic.lean
+++ ArtinWedderburn/Basic.lean
@@ -7,33 +7,6 @@ import Mathlib.Algebra.DirectSum.Module
 import Mathlib.RingTheory.SimpleModule.Basic
 -- Remove SimpleModule.Basic later when I work out how to bring just the defintions
 
-/-
-This section is on the Proof of Lemma 2 from the outline, which states
-
-Let S be a simple R-module and D = End_R(S). Define M = S^n. Then End_R(M) = M_n(D)
--/
-
-namespace Lemma2
-
---Define a ring R, a simple R-module S .
-variable (R : Type u_2) [Ring R] (S : Type u_4) [AddCommGroup S] [Module R S] [IsSimpleModule R S]
-
--- Define the Endomorphism ring of S and M = S^n
--- variable (D : Module.End R S)
-variable (M : DirectSum (Fin n) fun _ => S)
-
-/-
-Standard proof : End(M) is determined by the action on each summand S. So, look at the inclusion
-ιᵢ: S → M & projection πᵢ: M → S and consider f_ij = πᵢfιⱼ ∈ End(S) (= D by Schurs lemma).
-Elements of M are (s_1,…,s_n) so we can consider
-f(s_1,…,s_n) = (∑f_1j(s_j),…,∑f_nj(s_j)), but this is just the matrix representation.
-This is a homomorphism clearly, and simple to show its bijective.
-This gives us that End(M)≅Mₙ(End(S))≅Mₙ(D) by Schurs lemma.
--/
-
-
-end Lemma2
-
 namespace kernel_result
 
 -- Define two rings and a ring homomorphism between them.
@@ -65,3 +38,51 @@ theorem ker_hom_is_ideal : ideal {r : R | f r = 0} := by
   rw [hx, mul_zero]
 
 end kernel_result
+
+namespace Lemma2
+/-
+This section is on the Proof of Lemma 2 from the outline, which states:
+
+Thm: Let S be a simple R-module and D = End_R(S). Define M = S^n. Then End_R(M) = M_n(D)
+Proof : End(M) is determined by the action on each summand S. So, look at the inclusion
+ιᵢ: S → M & projection πᵢ: M → S and consider f_ij = πᵢfιⱼ ∈ End(S) (= D by Schurs lemma).
+Elements of M are (s_1,…,s_n) so we can consider
+f(s_1,…,s_n) = (∑f_1j(s_j),…,∑f_nj(s_j)), but this is just the matrix representation.
+This is a homomorphism clearly, and simple to show its bijective.
+This gives us that End(M)≅Mₙ(End(S))≅Mₙ(D) by Schurs lemma.
+
+This is still true without the simplicity assumption, so this is what we prove.
+-/
+
+def NEndEquivMatrixEnd
+    (n : ℕ) (R : Type) [Ring R] (S : Type) [AddCommGroup S] [Module R S] :
+    Module.End R (Fin n → S) ≃ Matrix (Fin n) (Fin n) (Module.End R S) where
+    --Def of forwards map
+  toFun F i j :=
+    {
+      toFun s := F (Pi.single j s) i
+      --Proof its linear
+      map_add' s t := by
+        sorry
+      map_smul' r s := by
+        sorry
+      }
+    --Def of reverse map
+  invFun M :=
+  {
+    toFun v i :=
+      ∑ j, M i j (v j)
+      --Proof its linear
+    map_add' v w := by
+      sorry
+    map_smul' r v := by
+      sorry
+  }
+  --Proof theyre inverse
+  left_inv := by
+    sorry
+  right_inv := by
+    sorry
+
+
+end Lemma2
